ff837589a9e58ce3a16f1dcfd768e5bc0185b25d
krishnas2 <krishnakanth1432@gmail.com>
2018-07-10 17:21:10 +0530

    Create GetRelatedController.js

---
+++ GetRelatedController.js
@@ -0,0 +1,8 @@
+({
+	doInit : function(component, event, helper){
+        helper.getAllTasks(component);
+    },
+    handleClick:function(component, event, helper){
+        helper.getAllTasks(component);
+    }
+})
